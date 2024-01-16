f7ad6a9fd05fea3da7d99c9a40c4b0301fa02f7b
Wrinkk <115036933+Wrinkk@users.noreply.github.com>
2024-01-16 18:54:30 +0900

    [Bronze I] Title: 세로읽기, Time: 212 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] arr = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String str = scanner.next();
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                if(arr[i][j]=='\0') continue;
+                System.out.print(arr[i][j]);
+            }
+        }
+    }
+}
